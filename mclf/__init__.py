954c95177b45b5d6f3e8f7bef62e54cfd73dc919
Stefan Wewers <stefan.wewers@uni-ulm.de>
2017-08-11 21:24:43 +0200

    __init__ updated

--- mclf/__init__.py
+++ mclf/__init__.py
@@ -1,8 +1,10 @@
 import sage.all
 from mac_lane import *
+from curves.smooth_projective_curves import SmoothProjectiveCurve
 from berkovich.berkovich_line import *
 from berkovich.berkovich_trees import *
 from berkovich.type_V_points import *
 from berkovich.affinoid_domain import *
 from semistable_reduction.superp import *
 from padic_extensions.weak_padic_galois_extensions import *
+from semistable_reduction.reduction_trees import ReductionTree, ReductionComponent
