1eadf80031e67be6c3dc3a7245560649749784db
Stefan Wewers <stefan.wewers@uni-ulm.de>
2017-10-13 13:31:28 +0200

    created new module superell

--- mclf/__init__.py
+++ mclf/__init__.py
@@ -6,6 +6,7 @@ from berkovich.berkovich_trees import *
 from berkovich.type_V_points import *
 from berkovich.affinoid_domain import *
 from semistable_reduction.superp import *
+from semistable_reduction.superell import *
 from padic_extensions.weak_padic_galois_extensions import *
 from padic_extensions.fake_padic_completions import *
 from padic_extensions.fake_padic_embeddings import *
