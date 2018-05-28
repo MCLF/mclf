70de14cbfd43139cfcc192413936cab94c04b7d3
Julian Rüth <julian.rueth@fsfe.org>
2018-05-28 17:15:58 +0200

    Do not pyflake __init__ files
    as they are using star imports and it would be quite annoying to change that

--- mclf/curves/__init__.py
+++ mclf/curves/__init__.py
@@ -1,6 +1,2 @@
 from .smooth_projective_curves import SmoothProjectiveCurve, PointOnSmoothProjectiveCurve
-SmoothProjectiveCurve; # silence pyflakes "unused import"
-PointOnSmoothProjectiveCurve; # silence pyflakes "unused import"
-
 from .morphisms_of_smooth_projective_curves import MorphismOfSmoothProjectiveCurves
-MorphismOfSmoothProjectiveCurves; # silence pyflakes "unused import"
