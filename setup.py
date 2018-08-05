74cba72eb867dac535d9fd30fc5d34d062e9e520
Julian Rüth <julian.rueth@fsfe.org>
2018-08-05 17:35:41 +0200

    Turns out you can not reupload to pypi
    and because I ran a first upload test, I trashed the 1.0.0 tag. Too bad, let's go with 1.0.1 then.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mclf",
-    version="1.0.0",
+    version="1.0.1",
     author="Stefan Wewers, Julian Rüth",
     author_email="stefan.wewers@uni-ulm.de",
     description="A Sage toolbox for computing with Models of Curves over Local Fields",
